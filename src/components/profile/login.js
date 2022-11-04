632ccbe513cd6397611a5d2290155c198c31433e
Farida Olia <farruolia@gmail.com>
2022-11-04 19:34:59 -0400

    a3 fall 2022

--- src/components/profile/login.js
+++ src/components/profile/login.js
@@ -23,6 +23,7 @@ export const Login = () => {
   const findAllUsers = () =>
     service.findAllUsers()
       .then(users => {
+          console.log(users)
         setExistingUsers(users)
       })
   const register = () =>
