67503170e77afd7b25dd873c7acdd8bec6f40ad2
Farida Olia <farruolia@gmail.com>
2022-11-20 16:50:17 -0500

    toggling-likes

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,7 +1,12 @@
 import axios from "axios";
-const BASE_URL = "https://farruolia-tuiter-backend.herokuapp.com"
+//const BASE_URL = "https://farruolia-tuiter-backend.herokuapp.com"
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 const TUITS_API = `${BASE_URL}/api/tuits`;
 const USERS_API = `${BASE_URL}/api/users`;
+
+const api = axios.create({
+    withCredentials: true
+});
 
 export const findAllTuits = () =>
   axios.get(TUITS_API)
@@ -12,12 +17,24 @@ export const findTuitById = (tid) =>
     .then(response => response.data);
 
 export const findTuitByUser = (uid) =>
+    api.get(`${USERS_API}/${uid}/tuits`)
+        .then(response => response.data);
+
+/*
+export const findTuitByUser = (uid) =>
   axios.get(`${USERS_API}/${uid}/tuits`)
     .then(response => response.data);
+ */
 
+/*
 export const createTuit = (uid, tuit) =>
   axios.post(`${USERS_API}/${uid}/tuits`, tuit)
     .then(response => response.data);
+*/
+
+export const createTuit = (uid, tuit) =>
+    api.post(`${USERS_API}/${uid}/tuits`, tuit)
+        .then(response => response.data);
 
 export const updateTuit = (tid, tuit) =>
   axios.post(`${TUITS_API}/${tid}`, tuit)
