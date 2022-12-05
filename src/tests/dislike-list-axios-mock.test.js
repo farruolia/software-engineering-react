a1518567578912223cb60526a4a8d16133424b1d
Farida Olia <farruolia@gmail.com>
2022-12-04 19:47:01 -0500

    big fixes

--- src/tests/dislike-list-axios-mock.test.js
+++ src/tests/dislike-list-axios-mock.test.js
@@ -1,8 +1,6 @@
 import {screen, render} from "@testing-library/react";
 import {HashRouter} from "react-router-dom";
 import React from "react";
-import * as dislikesService from "../services/dislikes-service";
-import MyDislikes from "../components/profile/my-dislikes";
 import axios from "axios";
 import TuitStats from "../components/tuits/tuit-stats";
 import {findAllTuitsDislikedByUser} from "../services/dislikes-service";
@@ -26,42 +24,6 @@ const MOCK_USERS = [
         username: 'charmi',
         password: 'charlie67',
         email: 'charlie@gmail.com'
-    }
-];
-
-const MOCKED_TUITS = [
-    {
-        _id: "123",
-        tuit: "alice's tuit",
-        postedBy: MOCK_USERS[1],
-        stats: {
-            replies: 0,
-            retuits: 0,
-            likes: 0,
-            dislikes: 4,
-        }
-    },
-    {
-        _id: "345",
-        tuit: "bob's tuit",
-        postedBy: MOCK_USERS[1],
-        stats: {
-            replies: 1,
-            retuits: 2,
-            likes: 0,
-            dislikes: 4,
-        }
-    },
-    {
-        _id: "890",
-        tuit: "charlie's tuit",
-        postedBy: MOCK_USERS[2],
-        stats: {
-            replies: 2,
-            retuits: 3,
-            likes: 0,
-            dislikes: 4,
-        }
     }
 ];
 
